796751dd216634e2a2c9951ad5fbaedb4d0c7ec8
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2017-02-27 14:00:23 +0100

    Allow to use any URL to access the service
    Closes #9

--- acceptance_tests/test_wms.py
+++ acceptance_tests/test_wms.py
@@ -9,3 +9,6 @@ def test_get_map(connection):
     if answer.headers["content-type"] != 'image/png':
         print(answer.text)
     assert answer.headers["content-type"] == 'image/png'
+
+def test_other_url(connection):
+    connection.get("toto/tutu?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=polygons&STYLES=&CRS=EPSG:4326&BBOX=-180,-90,180,90&WIDTH=600&HEIGHT=300&FORMAT=image/png")
