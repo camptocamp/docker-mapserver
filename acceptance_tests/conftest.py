b27d241e77fdeea1ba90ebdad9c15eb0047a34da
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2018-01-17 07:58:40 +0100

    Enabled no-cache
    Switched to c2cwsgiutils for acceptance tests.

--- acceptance_tests/conftest.py
+++ acceptance_tests/conftest.py
@@ -1,9 +1,16 @@
 """
 Common fixtures for every tests.
 """
+from c2cwsgiutils.acceptance import utils
+from c2cwsgiutils.acceptance.composition import Composition
+from c2cwsgiutils.acceptance.connection import Connection
 import pytest
 
-from acceptance_tests import Composition, Connection, wait_mapserver
+BASE_URL = 'http://' + utils.DOCKER_GATEWAY + ':8380/'
+
+
+def wait_mapserver():
+    utils.wait_url(BASE_URL + '?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&TYPENAME=polygons&featureId=xxx')
 
 
 @pytest.fixture(scope="session")
@@ -11,7 +18,9 @@ def composition(request):
     """
     Fixture that start/stop the Docker composition used for all the tests.
     """
-    return Composition(request)
+    result = Composition(request, 'mapserver', 'docker-compose.yml')
+    wait_mapserver()
+    return result
 
 
 @pytest.fixture
@@ -19,4 +28,4 @@ def connection(composition):
     """
     Fixture that returns a connection to a running batch container.
     """
-    return Connection(composition)
+    return Connection(BASE_URL, 'http://localhost')
--- acceptance_tests/utils.py
+++
@@ -1,4 +0,0 @@
-import os
-
-def in_docker():
-    return os.environ.get("DOCKER_RUN", "0") == "1"
