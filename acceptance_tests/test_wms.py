f3b17dd341976cb628a9ee8b9b4fb3cc498c5f8d
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2018-01-17 07:58:40 +0100

    Enabled no-cache
    Switched to c2cwsgiutils for acceptance tests.

--- acceptance_tests/test_wms.py
+++ acceptance_tests/test_wms.py
@@ -5,7 +5,7 @@ def test_get_capabilities(connection):
     assert [e.text for e in answer.findall(".//%sLayer/%sName" % (ns, ns))] == ['test', 'polygons']
 
 def test_get_map(connection):
-    answer = connection.get("?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=polygons&STYLES=&CRS=EPSG:4326&BBOX=-180,-90,180,90&WIDTH=600&HEIGHT=300&FORMAT=image/png")
+    answer = connection.get_raw("?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=polygons&STYLES=&CRS=EPSG:4326&BBOX=-180,-90,180,90&WIDTH=600&HEIGHT=300&FORMAT=image/png")
     if answer.headers["content-type"] != 'image/png':
         print(answer.text)
     assert answer.headers["content-type"] == 'image/png'
