60b89c2be33f4b296f81391fea93b9694cc970d8
Stéphane Brunner <stephane.brunner@camptocamp.com>
2021-06-03 15:18:55 +0200

    Fix dash in MS_MAP_PATTERN

--- acceptance_tests/test_map.py
+++ acceptance_tests/test_map.py
@@ -1,7 +1,25 @@
-def test_get_capabilities(connection_map):
+def test_get_std_map(connection_map):
     ns = "{http://www.opengis.net/wms}"
     answer = connection_map.get_xml(
         "?MAP=/etc/mapserver/mapserver.map&SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0"
+    )
+    assert [e.text for e in answer.findall("%sService/%sTitle" % (ns, ns))] == ["test"]
+    assert [e.text for e in answer.findall(".//%sLayer/%sName" % (ns, ns))] == ["test", "polygons"]
+
+
+def test_get_multi_char_map(connection_map):
+    ns = "{http://www.opengis.net/wms}"
+    answer = connection_map.get_xml(
+        "?MAP=/etc/mapserver/AZ_az-0.9.map&SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0"
+    )
+    assert [e.text for e in answer.findall("%sService/%sTitle" % (ns, ns))] == ["test"]
+    assert [e.text for e in answer.findall(".//%sLayer/%sName" % (ns, ns))] == ["test", "polygons"]
+
+
+def test_get_folder_map(connection_map):
+    ns = "{http://www.opengis.net/wms}"
+    answer = connection_map.get_xml(
+        "?MAP=/etc/mapserver/AZ_az-0.9/AZ_az-0.9.map&SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0"
     )
     assert [e.text for e in answer.findall("%sService/%sTitle" % (ns, ns))] == ["test"]
     assert [e.text for e in answer.findall(".//%sLayer/%sName" % (ns, ns))] == ["test", "polygons"]
