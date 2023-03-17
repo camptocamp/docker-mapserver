b40618fce845a17a30a6056139fccd0a8b01d2b2
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-03-17 16:14:42 +0100

    Fix new error with attr in pull request check workflow

--- builder/entry.sh
+++ builder/entry.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -e
 
-if [ "$GID" != "0" ]
-then
+if [ "$GID" != "0" ]; then
     groupadd -r -g $GID builders
     GROUP=builders
 else
     GROUP=root
 fi
 
-if [ "$UID" != "0" ]
-then
+if [ "$UID" != "0" ]; then
     useradd -m builder -u $UID -g $GROUP
     USER=builder
     chown -R builder: /home/builder
