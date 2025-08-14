3529fbcd2fe36647d7775881ae2079f767823e99
Stéphane Brunner <stephane.brunner@camptocamp.com>
2025-08-14 15:42:37 +0200

    fix

--- runtime/etc/lighttpd/path_info.lua
+++ runtime/etc/lighttpd/path_info.lua
@@ -1,14 +1,5 @@
-local base_path = os.getenv("MAPSERVER_BASE_PATH") or "/"
-
-print("MAPSERVER_BASE_PATH: " .. base_path)
-
 if lighty.r.req_attr["request.path-info"] then
-    print("path-info: " .. lighty.r.req_attr["request.path-info"]  or "")
-    print("path: " .. lighty.r.req_attr["uri.path"]  or "")
-    lighty.r.req_attr["uri.path"] = base_path .. lighty.r.req_attr["request.path-info"]
-    lighty.r.req_attr["uri.path-raw"] = base_path .. lighty.r.req_attr["request.path-info"]
-    lighty.r.req_attr["request.path-info"] = base_path .. lighty.r.req_attr["request.path-info"]
-    lighty.r.req_env["PATH_INFO"] = base_path .. lighty.r.req_attr["request.path-info"]
-    print("path: " .. lighty.r.req_attr["uri.path"]  or "")
-    print("path-info: " .. lighty.r.req_attr["request.path-info"]  or "")
+    local base_path = os.getenv("MAPSERVER_BASE_PATH", "")
+    local path_info = lighty.r.req_attr["request.path-info"]
+    lighty.r.req_attr["uri.path"] = base_path .. path_info
 end
